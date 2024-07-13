16ba9ae33f4a3f8a80d67d462ddbaa9fc58e7729
Alex Mikhalochkin <45179689+AlexMikhalochkin@users.noreply.github.com>
2024-07-13 15:44:27 +0300

    Refine script for DigitalOcean firewall update (#2)

--- script.sh
+++ script.sh
@@ -5,13 +5,18 @@ check_connection() {
   return $?
 }
 
-# Function to update firewall rule using doctl
 update_firewall() {
   current_ip=$(curl -s https://ipinfo.io/ip)
-  doctl compute firewall add-rules "$DIGITAL_OCEAN_FIREWALL_ID" \
+  /app/doctl compute firewall add-rules "$DIGITAL_OCEAN_FIREWALL_ID" \
     --inbound-rules "protocol:tcp,ports:1883,address:$current_ip"
-  echo "$(date): Firewall is updated. New IP: [$current_ip]"
+  if [ $? -eq 0 ]; then
+    echo "$(date): Firewall is updated. New IP: [$current_ip]"
+  else
+    echo "$(date): Failed to update the firewall."
+  fi
 }
+
+# TODO add validation of environment variables
 
 while true; do
   if ! check_connection; then
